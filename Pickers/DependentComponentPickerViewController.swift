1001bb28ef33383c89c50c52da0aa255d93fc99d
sampatelstudy <114133445+sampatelstudy@users.noreply.github.com>
2022-10-23 22:31:43 -0400

    first commit

---
+++ Pickers/DependentComponentPickerViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DependentComponentPickerViewController.swift
+//  Pickers
+//
+//  Created by Samir Patel on 2022-10-23.
+//
+
+import UIKit
+
+class DependentComponentPickerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
